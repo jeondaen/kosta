05a72c42edce5da38a693e97c07d72fa50465dcd
jeondaen <jeondaen@gmail.com>
2019-11-23 18:30:04 +0900

    study at home

---
+++ houseWork/kp.js
@@ -0,0 +1,14 @@
+	// Window 내에서 키를 눌렀을때 발생하는 이벤트
+
+	window.onkeydown = function(event){
+
+		if(event.keyCode == 115) {
+			
+			window.open("kp2.html", "_blank", "width=400, height=300, left=500, top=400");
+			
+		}
+
+	}
+
+
+	
---
+++ houseWork/kp2.js
@@ -0,0 +1,3 @@
+window.onload = function(){
+	var kp2m = document.getElementById("kp2_main").focus();
+}
