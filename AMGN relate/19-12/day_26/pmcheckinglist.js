8d28796f02faa7a7f6e6aa5165d82671f718e0cf
jeondaen <jeondaen@gmail.com>
2019-12-28 16:32:56 +0900

    아 논현동 가기 귀찮다.

--- AMGN relate/19-12/day_26/pmcheckinglist.js
+++ AMGN relate/19-12/day_26/pmcheckinglist.js
@@ -23,8 +23,9 @@ card_num
 ========================
 
 */
-
 var xhrc;
+
+var cardno;
 
 function createXHR() {
   return new XMLHttpRequest();
@@ -71,6 +72,7 @@ var checkTool = (function() {
           프로그레스 바 핸들 함수 NEEDS
           
           */
+          cardno = card_num;
 
           let checkDTO = JSON.parse(xhrc.response);
           let listOfCheck = checkDTO.checkLists;
@@ -84,33 +86,59 @@ var checkTool = (function() {
     xhrc.send();
   }
 
-  function modCheckList(arg) {
-    console.log(arg);
+  function modCheckList(...arg) {
     xhrc = createXHR();
 
+    let len = arg.length;
+    let checkObj;
+    switch (len) {
+      case 2:
+        checkObj = { checklist_num: arg[0], checklist_check: arg[1] };
+        checkObj = JSON.stringify(checkObj);
+        break;
+
+      case 3:
+        checkObj = {
+          checklist_num: arg[0],
+          checklist_check: arg[1],
+          checklist_content: arg[2]
+        };
+        checkObj = JSON.stringify(checkObj);
+        break;
+    }
+
+    console.log(checkObj);
     xhrc.onreadystatechange = function() {
       if (xhrc.readyState == 4) {
         if (xhrc.status == 200) {
           // 클릭한 대상의 checked 와 innerHTML 을 캐치
+          listCheckList(cardno);
         }
       }
     };
+
+    xhrc.open("POST", "/checkList/mod");
+    xhrc.setRequestHeader("Content-Type", "application/json");
+    xhrc.send(checkObj);
   }
   function delCheckList() {}
   /*
    체크리스트 노드 생성
   */
   function pm_createCheckList(pm_card_num, pm_checklist) {
-    var form_group = document.querySelector(".formgroup"); // 최상위 체크박스 항목
+    var form_group = document.querySelector(".pm_totalCheckList"); // 최상위 체크박스 항목
     form_group.innerHTML = "";
 
     for (let elem of pm_checklist) {
+      var paCheckGroup = document.createElement("div"); // 새로운 체크리스트1
+      paCheckGroup.className = "paCheckGroup";
+
       var paChecklistLine = document.createElement("div"); // 하나의 체크리스트 항목
       paChecklistLine.className =
         "custom-control custom-checkbox paChecklistLine";
 
       var paChecklistLineCon = document.createElement("div"); // 체크리스트 내부 컨텐츠 시작
-      paChecklistLineCon.className = "paChecklistLineCon";
+      paChecklistLineCon.className = "paChecklistLineCon pm_checkEventListener";
 
       var paCheckControlBox = document.createElement("input");
       paCheckControlBox.type = "checkbox";
@@ -132,8 +160,43 @@ var checkTool = (function() {
       paChecklistLineCon.append(paCheckItemMenu);
 
       paChecklistLine.append(paChecklistLineCon);
-      console.log(elem);
-      form_group.append(paChecklistLine);
+
+      var paCardCheckIputBox = document.createElement("div");
+      paCardCheckIputBox.className = "paCardCheckIputBox cf";
+
+      var paCheckInput = document.createElement("textarea");
+      paCheckInput.className = "paCheckInput";
+      //paCheckInput.value = "";
+      paCardCheckIputBox.append(paCheckInput);
+
+      var nully = document.createElement("button");
+      nully.type = "button";
+      nully.id = "null";
+      nully.className = "btn btn-success paCheckInputBtn";
+      nully.innerHTML = "저장";
+      paCardCheckIputBox.append(nully);
+
+      var paCheckInputDelBtn = document.createElement("span");
+      paCheckInputDelBtn.className = "paCheckInputDelBtn";
+
+      var img1 = document.createElement("img");
+      img1.src = "/resources/images/board_w_traschcan.png";
+      img1.alt = "";
+      img1.className = "paDelImage1";
+      paCheckInputDelBtn.append(img1);
+
+      var img2 = document.createElement("img");
+      img2.src = "/resources/images/board_w_traschcan2.png";
+      img2.alt = "";
+      img2.className = "paDelImage2";
+      paCheckInputDelBtn.append(img2);
+
+      paCardCheckIputBox.append(paCheckInputDelBtn);
+
+      paCheckGroup.append(paChecklistLine);
+      paCheckGroup.append(paCardCheckIputBox);
+
+      form_group.append(paCheckGroup);
     }
   }
 
@@ -143,10 +206,95 @@ var checkTool = (function() {
 
   function pm_handleProgress(totalCheck, doneCheck, target) {
     let x = (doneCheck * 100) / totalCheck;
+    if (x == Infinity) x = 0;
 
     var progress_bar = document.querySelector(".progress-bar");
-    progress_bar.style.width = x;
+    progress_bar.style.width = x + "%";
   }
 
-  return { insCheckList: insCheckList, listCheckList: listCheckList };
+  return {
+    insCheckList: insCheckList,
+    listCheckList: listCheckList,
+    modCheckList: modCheckList
+  };
 })();
+
+window.onload = function() {
+  // PM_REPLY.JS 파일에서 window.onload 가 먹히지 않아 이부분에 댓글을 실행하는 코드를 작성함 =====================
+
+  var commentInsert = document.querySelector(
+    ".paCommentInputBtn"
+  ); /* INSERT 버튼을 눌렀을때 ( 추가기능 ) */
+  console.log("pm_reply area" + commentInsert);
+  commentInsert.addEventListener("click", function() {
+    if (document.querySelector(".paCommentInput").value.trim() != "") {
+      console.log("적용");
+      insReply();
+    }
+  });
+
+  // 체크리스트의 내용이 변화하였을 때
+  $(".pm_totalCheckList").on("change", ".pm_checkEventListener", function() {
+    let tf = this.querySelector("input").checked;
+    let ckl_num = this.querySelector("input").id;
+    let ckl_content = this.querySelector("label").innerHTML;
+
+    console.log(tf, ckl_num, ckl_content);
+    checkTool.modCheckList(ckl_num, tf, ckl_content);
+  });
+
+  //클릭시 감추기
+  $(".paCheckGroup").on("click", ".paCheckItemMenu", function() {
+    console.log($(this));
+    $(".paChecklistLine").hide();
+    $(".paCardCheckIputBox").show();
+  });
+
+  //다른 곳 클릭시 닫히기
+  $("html").mouseup(function(e) {
+    if (!$(".paCardCheckIputBox *").is(e.target)) {
+      $(".paChecklistLine").show();
+      $(".paCardCheckIputBox").hide();
+    }
+  });
+
+  //체크리스트 입력창 자동 조절
+  $(".paCheckInput").on("keydown keyup", function() {
+    $(this)
+      .height(1)
+      .height($(this).prop("scrollHeight") - 14);
+  });
+
+  //체크리스트 추가 버튼
+  $(".paCheckWorkBtn").on("click", ".paCheckWorkBtn", function() {
+    console.log("kkk");
+    $(".paCardCheckIAddBox").show();
+    $(".paCheckWorkBtn").hide();
+  });
+  //체크리스트 추가 입력창 조절
+  $(".paCheckAddInput").on("keydown keyup", function() {
+    $(this)
+      .height(1)
+      .height($(this).prop("scrollHeight") - 14);
+  });
+  //체크리스트 추가 꺼짐
+  $("html").mouseup(function(e) {
+    if (!$(".paCardCheckIAddBox *").is(e.target)) {
+      $(".paCheckWorkBtn").show();
+      $(".paCardCheckIAddBox").hide();
+    }
+  });
+
+  $(".paGetCheck").change(function() {
+    if ($paGetCheck.is(":checked")) {
+      //체크박스에 체크 된 경우
+      $paGetCheck.addClass("on");
+      $paDudateComplete.show();
+      $paDudateOverdue.hide();
+    } else {
+      //체크박스에 체크되지 않은 경
+      $paDudateComplete.hide();
+      $paDudateOverdue.show();
+    }
+  });
+};
